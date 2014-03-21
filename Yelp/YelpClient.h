ca33ff530ce830b0152bf422a885bea859ddcf6e
Timothy Lee <timothy1ee@yahoo.com>
2014-03-21 02:07:37 -0700

    Minor cleanup.

--- Yelp/YelpClient.h
+++ Yelp/YelpClient.h
@@ -13,6 +13,6 @@
 
 - (id)initWithConsumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret accessToken:(NSString *)accessToken accessSecret:(NSString *)accessSecret;
 
-- (AFHTTPRequestOperation *)searchWithTerm:(NSString *)term success:(void (^)(AFHTTPRequestOperation *operation, id response))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;;
+- (AFHTTPRequestOperation *)searchWithTerm:(NSString *)term success:(void (^)(AFHTTPRequestOperation *operation, id response))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
 
 @end
