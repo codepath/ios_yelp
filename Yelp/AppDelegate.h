be9d1d6b2c686172267740a98ccf19150a64ab64
Timothy Lee <timothy1ee@yahoo.com>
2014-03-21 00:57:33 -0700

    Initial Commit

---
+++ Yelp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Yelp
+//
+//  Created by Timothy Lee on 3/21/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YelpTests/YelpTests.m
@@ -0,0 +1,34 @@
+//
+//  YelpTests.m
+//  YelpTests
+//
+//  Created by Timothy Lee on 3/21/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YelpTests : XCTestCase
+
+@end
+
+@implementation YelpTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
