c0dcae4cbf5b565502a2b7c4afb47d4d2a1c62a6
Timothy Lee <tim@codepath.com>
2014-10-24 00:51:54 -0700

    Change default location to lat/long

--- Yelp/YelpClient.m
+++ Yelp/YelpClient.m
@@ -23,7 +23,7 @@
 - (AFHTTPRequestOperation *)searchWithTerm:(NSString *)term success:(void (^)(AFHTTPRequestOperation *operation, id response))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure {
     
     // For additional parameters, see http://www.yelp.com/developers/documentation/v2/search_api
-    NSDictionary *parameters = @{@"term": term, @"location" : @"San Francisco"};
+    NSDictionary *parameters = @{@"term": term, @"ll" : @"37.774866,-122.394556"};
     
     return [self GET:@"search" parameters:parameters success:success failure:failure];
 }
